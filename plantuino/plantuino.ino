025bd212391baba8a11c1e64d3c05538ee286636
diesanz91 <diego_sanz@live.com.ar>
2017-06-20 21:11:05 -0300

    Se comento y limpio codigo, se agregaron algunos cambios para permitir modificar parametros limites de los sensores

--- plantuino/plantuino.ino
+++ plantuino/plantuino.ino
@@ -1,71 +1,69 @@
-
-//*** DHT AM2302 *****************************
+//*** Sensor de temperatura y humedad - DHT AM2302 ***//
 #include "DHT.h"
-#define DHTPIN 2     // what pin we're connected to
-// Uncomment whatever type you're using!
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
-// Initialize DHT sensor for normal 16mhz Arduino
-DHT dht(DHTPIN, DHTTYPE);
+#define DHTPIN 2                // Definimos PIN al que nos vamos a conectar
+#define DHTTYPE DHT22           // Definimos tipo de sensor que tenemos DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE);       // Inicializamos sensor DHT para Arduino en 16mhz
 
-//*** BH1750 *********************************
+//*** Sensor de luz - BH1750 ***//
 #include <Wire.h>
 #include <BH1750.h>
 BH1750 lightMeter;
 
+//*** Sensor Bluetooth - HC-05 ***//
+#include <SoftwareSerial.h>     // Incluimos la librería  SoftwareSerial  
+SoftwareSerial BT(10,11);       // Definimos los pines RX y TX del Arduino conectados al Bluetooth
 
-//*** HC-05 **********************************
-#include <SoftwareSerial.h>   // Incluimos la librería  SoftwareSerial  
-SoftwareSerial BT(10,11);    // Definimos los pines RX y TX del Arduino conectados al Bluetooth
-
-//*** Parametros **********************************
+//*** Parametros ***//
 #define LED 13
-#define WAT 8
-#define FAN 7
+#define WAT 8                   // Definimos PIN para activar/desactivar bomba de agua
+#define FAN 7                   // Definimos PIN para activar/desactivar ventilador
 
-byte pinEstado = 0; //Para ver si se activa un pin porque se detecta una medida limite (todavia no se usa)
-byte pinEstadoApp = 0; //Para ver si se activa un pin porque se dio la orden desde la App (todavia no se usa)
+byte pinEstado = 0;             // Para ver si se activa un pin porque se detecta una medida limite (todavia no se usa)
+byte pinEstadoApp = 0;          // Para ver si se activa un pin porque se dio la orden desde la App (todavia no se usa)
 
-#define limiteLuz 30 //Limite de luz
-#define limiteHumedad 50 //Limite de humedad
-#define limiteTemperatura 25 //Limite de temperatura
-#define tiempo_lectura 5000 //Cada cuantos milisegundos lee los sensores para activar o no los pines
-int tiempo = tiempo_lectura; // Para que lea los sensores cuando prende la placa
+//*** Definición de límites ***//
+#define limiteLuz 30            // Limite de luz, medido en lx
+#define limiteHumedad 50        // Limite de humedad, medido en %
+#define limiteTemperatura 25    // Limite de temperatura, medido en °C
+#define tiempo_lectura 5000     // Cada cuantos milisegundos lee los sensores para activar o no los pines
+int tiempo = tiempo_lectura;    // Para que lea los sensores cuando prende la placa
 
 void setup() {
-  BT.begin(9600);       // Inicializamos el puerto serie BT (Para Modo AT 2)
-  BT.flush();           // Para borrar la chache del puerto serie
-  delay(500);           // Para darle tiempo que inicie el modulo BT
   
-  Serial.begin(9600);   // Inicializamos  el puerto serie 
+  BT.begin(9600);               // Inicializamos el puerto serie BT (Para Modo AT 2)
+  BT.flush();                   // Para borrar la chache del puerto serie
+  delay(500);                   // Para darle tiempo que inicie el modulo BT
+  
+  Serial.begin(9600);           // Inicializamos  el puerto serie 
 
-  pinMode(LED, OUTPUT); //Define el pin para prender la luz
+  pinMode(LED, OUTPUT);         //Define el pin para prender la luz
   digitalWrite(LED,LOW);
-  pinMode(WAT, OUTPUT); //Define el pin para prender la bomba de agua
+  pinMode(WAT, OUTPUT);         //Define el pin para prender la bomba de agua
   digitalWrite(WAT,LOW);
-  pinMode(FAN, OUTPUT); //Define el pin para prender el ventilador
+  pinMode(FAN, OUTPUT);         //Define el pin para prender el ventilador
   digitalWrite(FAN,LOW);
       
   Serial.println("Inicio Plantuino");
  
   dht.begin();
   lightMeter.begin();
+  
 }
 
 void loop() {
 
-  loop_bluetooth(); //Lee si se reciben datos de bluetooth desde la App
+  loop_bluetooth();             //Lee si se reciben datos de bluetooth desde la App
   
   delay(100);
-  tiempo += 100; //Contador de tiempo, porque la frecuencia de letura de bluetooth es mayor que la frecuencia de lectura de los sensores
+  tiempo += 100;                //Contador de tiempo, porque la frecuencia de letura de bluetooth es mayor que la frecuencia de lectura de los sensores
 
-  
   if (tiempo > tiempo_lectura){ //Lee los sensores cuando se cumple el tiempo seteado
  
-    tiempo = 0; //Reinicia el tiempo
+    tiempo = 0;                 //Reinicia el tiempo
 
     uint16_t lux = lightMeter.readLightLevel(); //Lee sensor luz
-    float h = dht.readHumidity(); //Lee sensor humedad
-    float t = dht.readTemperature(); //Lee sensor temperatura
+    float h = dht.readHumidity();               //Lee sensor humedad
+    float t = dht.readTemperature();            //Lee sensor temperatura
     
     Serial.print("Luz: ");
     Serial.print(lux);
@@ -86,102 +84,135 @@ void loop() {
 
 }
 
-//Funcion para cambiar de esado un pin
+// Función para cambiar de estado un pin
 void toggle(int pinNum)
 {
   pinEstadoApp = !pinEstadoApp;
   digitalWrite(pinNum, pinEstadoApp);
 }
 
+// Función para leer indicaciones desde Aplicacion Android
 void loop_bluetooth()
 {
 
-  if(BT.available()) //Verifica si se recibieron datos de bluetooth
+  if(BT.available())            // Verifica si se recibieron datos de bluetooth
   {
     char command = BT.read();
     BT.flush();
     Serial.println(command);
 
-    switch(command){ //Lee el comando recibido
+    //String parametros = String(command);
+
+    //String limLuz = getValue(command,'-', 1);
+    //Serial.print("Limite Luz: ");
+    //Serial.println(limLuz);
+
+    //String limTemp = getValue(command,'-', 2);
+    //Serial.print("Limite Temperatura: ");
+    //Serial.println(limTemp);
+
+    //String limHum = getValue(command,'-', 3);
+    //Serial.print("Limite Humedad: ");
+    //Serial.println(limHum);
+
+    //command = getValue(command,'-', 0);
+
+    switch(command){            //Lee el comando recibido
+      //case 'P':
+        //Serial.println(limLuz);
+        //Serial.println(limTemp);
+        //Serial.println(limHum);
+        //break;
       case 'V':
-        //Encender Ventilacion
+        Serial.println("Encender Ventilacion");
+        digitalWrite(FAN,HIGH);       
         break;
       case 'v':
-        //Apagar Ventilacion
+        Serial.println("Apagar Ventilacion");
+        digitalWrite(FAN,LOW);
         break;
       case 'I':
-        //Encender iluminacion
-        toggle(LED); //Enienda/Apaga LED
+        Serial.println("Encender iluminacion");
+        toggle(LED);            //Enienda/Apaga LED
         break;
       case 'i':
-        //Apagar iluminacion
-        toggle(LED); //Enienda/Apaga LED
+        Serial.println("Apagar iluminacion");
+        toggle(LED);            //Enienda/Apaga LED
         break;
       case 'B':
-        //Encender bomba de agua
+        Serial.println("Encender bomba de agua");
+        digitalWrite(WAT,HIGH);
         break;
       case 'b':
-        //Apagar bomba de agua
+        Serial.println("Apagar bomba de agua");
+        digitalWrite(WAT,LOW);
         break;
       case 'L':
-        loop_light(); //Lee sensor luz y envia datos por bluetooth a la App
+        loop_light();           //Lee sensor luz y envia datos por bluetooth a la App
         break;
       case 'T':
-        loop_temperature(); //Lee sensor temperatura y envia datos por bluetooth a la App
+        loop_temperature();     //Lee sensor temperatura y envia datos por bluetooth a la App
         break;  
       case 'H':
-        loop_humidity(); //Lee sensor humedad y envia datos por bluetooth a la App
+        loop_humidity();        //Lee sensor humedad y envia datos por bluetooth a la App
         break;
       case 'O':
-        toggle(LED); //Enienda/Apaga LED
+        toggle(LED);            //Enienda/Apaga LED
         break;           
-      case 'A':  //Lee todos los sensores y envia datos por bluetooth a la App
+      case 'A':                 //Lee todos los sensores y envia datos por bluetooth a la App
         loop_light();
         loop_temperature();
         loop_humidity();
         break;           
     }
   }
-      
-  if(Serial.available())  // Si llega un dato por el monitor serial se envía al puerto BT
+  
+  /* 
+  if(Serial.available())       // Si llega un dato por el monitor serial se envía al puerto BT
   {
      BT.write(Serial.read());
   } 
-     
+  */
+  
 }
 
 void loop_light() {
-  uint16_t lux = lightMeter.readLightLevel();
-  BT.print("Sensor Luz: ");
+  
+  uint16_t lux = lightMeter.readLightLevel();   // Leemos nivel de luz
+  
+  BT.print("Light Sensor: ");
   BT.print(lux);
   BT.println(" lx");
+
 }
 
 void loop_temperature() {
-  float t = dht.readTemperature();
+  
+  float t = dht.readTemperature();              // Leemos temperatura
 
-  // Check if any reads failed and exit early (to try again).
+  // Chequeamos si falló al leer sensor y salimos para intentar nuevamente
   if (isnan(t)) {
-    Serial.println("Failed to read from DHT sensor!");
+    Serial.println("Falló al leer desde sensor DHT!");
     return;
   }
 
-  BT.print("Sensor Temperatura: "); 
+  BT.print("Temperature Sensor: "); 
   BT.print(t);
-  BT.println(" C");
-
+  BT.println(" °C");
+  
 }
 
 void loop_humidity() {
-  float h = dht.readHumidity();
+  
+  float h = dht.readHumidity();                 //Leemos humedad
 
-  // Check if any reads failed and exit early (to try again).
+  // Chequeamos si falló al leer sensor y salimos para intentar nuevamente
   if (isnan(h)) {
-    Serial.println("Failed to read from DHT sensor!");
+    Serial.println("Falló al leer desde sensor DHT!");
     return;
   }
 
-  BT.print("Sensor Humedad: "); 
+  BT.print("Humidity sensor: "); 
   BT.print(h);
   BT.println(" %\t");
 
